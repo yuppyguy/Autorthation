c4b61a5289379880303c0d939cdf862a6fa4f25a
yuppyguy <smur4ick@gmail.com>
2023-04-06 23:08:08 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 var expression = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 var email = document.getElementById('email');
 if (expression.test('email') === true) {
-    console.log('email is correct');
+    email.style.backgroundColor = 'green';
 }
 else {
-    alert('email is incorrect');
+    email.style.backgroundColor === 'red';
+}
+var password = document.getElementById('psw');
+var repeatPassword = document.getElementById('psw-repeat');
+function validatePassword(password) {
+    var passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    return passwordRegex.test(password);
+}
+if (validatePassword(password)) {
+    password.style.backgroundColor = 'green';
+}
+else {
+    password.style.backgroundColor = 'red';
+}
+if (repeatPassword === password) {
+    repeatPassword.style.backgroundColor = 'green';
+}
+else {
+    repeatPassword.style.backgroundColor = 'red';
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
-const expression: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-let email:HTMLFormElement = document.getElementById('email') as HTMLFormElement;
+const expression: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+const email:HTMLFormElement = document.getElementById('email') as HTMLFormElement
 if (expression.test('email') === true) {
-    console.log('email is correct')
+    email.style.backgroundColor = 'green'
 }
 else {
-    alert('email is incorrect')
+    email.style.backgroundColor === 'red'
+}
+
+const password:HTMLFormElement = document.getElementById('psw') as HTMLFormElement
+const repeatPassword:HTMLFormElement = document.getElementById('psw-repeat') as HTMLFormElement
+
+function validatePassword(password): boolean {
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    return passwordRegex.test(password);
+}
+
+if (validatePassword(password)) {
+    password.style.backgroundColor = 'green'
+} else{
+    password.style.backgroundColor = 'red'
+}
+
+if (repeatPassword === password) {
+    repeatPassword.style.backgroundColor = 'green'
+} else{
+    repeatPassword.style.backgroundColor = 'red'
 }
